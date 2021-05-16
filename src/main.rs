c3d7de7036668f450115cf29b01db4bf93c02a52
eweca1992 <dreamenako@outlook.com>
2021-05-16 20:52:05 +0800

    apply xmlhttprequest method

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,81 @@
-use bytes::Bytes;
+use futures::stream::TryStreamExt;
 use glob::glob;
 use percent_encoding::percent_decode;
 use std::fs;
 use std::io::prelude::*;
 use std::path::Path;
-use warp::{http::Response, Filter};
+use warp::{http::Response, Buf, Filter};
 
-const ELEMN: usize = 82;
-const EWSTR: &str = r#"name="ew" size="1%" value="0""#;
-const YYSTR: &str = r#"name="yy" size="1%" value="0""#;
 const LOADSTR: &str = r#"<option value="%forbidden%">content</option>"#;
 
 #[tokio::main]
 async fn main() {
-    let default_reply = warp::path::end().map(|| Response::builder().body(default_reply()));
+    let default_reply = warp::path::end()
+        .and(warp::get())
+        .map(|| Response::builder().body(default_reply()));
 
     let post_reply = warp::post()
-        .and(warp::body::bytes())
-        .map(|content: Bytes| Response::builder().body(parse_post(content)));
+        .and(warp::multipart::form())
+        .and_then(post_reply);
 
     let routers = warp::get().and(default_reply).or(post_reply);
 
     warp::serve(routers).run(([127, 0, 0, 1], 3030)).await;
 }
 
-fn parse_post(content: Bytes) -> String {
-    let content = percent_decode(&content[..]).decode_utf8().unwrap();
-    let content_vec = content.split("&").collect::<Vec<&str>>();
+async fn post_reply(form: warp::multipart::FormData) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut parts: Vec<warp::multipart::Part> = form
+        .try_collect()
+        .await
+        .map_err(|_e| warp::reject::reject())?;
 
-    let name = content_vec[0].split("=").collect::<Vec<&str>>()[1];
+    if parts[0].name() == "save" {
+        let buf = parts[0].data().await.unwrap().unwrap();
+        let file_name = percent_decode(&buf.chunk()[..])
+            .decode_utf8()
+            .unwrap()
+            .to_string();
 
-    if content_vec[0].starts_with("save") {
-        let mut ew = ["0"; ELEMN];
-        let mut yy = ["0"; ELEMN];
-        for (idx, elem) in content_vec[2..ELEMN * 2 + 2 + 1].iter().enumerate() {
-            let elem_vec = elem.split("=").collect::<Vec<&str>>();
-            if idx / ELEMN == 0 {
-                ew[idx] = elem_vec[1];
-            } else if idx != ELEMN {
-                let idx2 = idx - 1;
-                yy[idx2 % ELEMN] = elem_vec[1];
+        let mut ew = None;
+        let mut yy = None;
+        for part in parts[1..].iter_mut() {
+            let key = part.name().to_string();
+            let buf = part.data().await.unwrap().unwrap();
+            let value = percent_decode(&buf.chunk()[..])
+                .decode_utf8()
+                .unwrap()
+                .to_string();
+
+            if key == "ew" {
+                ew = Some(value);
+            } else if key == "yy" {
+                yy = Some(value);
             }
         }
 
-        record(name, ew, yy);
+        if ew.is_some() && yy.is_some() {
+            record(file_name, ew.unwrap().to_string(), yy.unwrap().to_string());
+        }
 
-        // renew code
-        let name = "index.html";
-        let path_string = "./".to_string() + &name;
+        Ok(Response::builder().body("OK".to_string()))
+    } else if parts[0].name() == "load" {
+        let buf = parts[0].data().await.unwrap().unwrap();
+        let file_name = percent_decode(&buf.chunk()[..])
+            .decode_utf8()
+            .unwrap()
+            .to_string();
 
-        let mut contents = fs::read_to_string(path_string).unwrap();
-        contents = show_load_files(contents);
-
-        contents
-    } else if content_vec[0].starts_with("load") {
-        let name: String = name.to_string() + ".ini";
-        let path_string = "./".to_string() + &name;
-
+        let path_string = "./".to_string() + &file_name + &".ini";
         let contents = fs::read_to_string(path_string).unwrap();
-        let contents = contents.split(",").collect::<Vec<&str>>();
 
-        let mut ew = ["0"; ELEMN];
-        let mut yy = ["0"; ELEMN];
-        if contents.len() != ELEMN * 2 {
-            println!("申请读取一份无效的或者被错误修改过的预设文件。");
-        } else {
-            for (idx, elem) in contents.iter().enumerate() {
-                if idx / ELEMN == 0 {
-                    ew[idx] = elem;
-                } else {
-                    yy[idx % ELEMN] = elem;
-                }
-            }
-        }
-
-        let name = "index.html";
-        let path_string = "./".to_string() + &name;
-
-        let mut contents = fs::read_to_string(path_string).unwrap();
-        contents = show_load_files(contents);
-
-        // modify contents
-        for i in 0..ELEMN {
-            // replace the value
-            if ew[i] != "0" {
-                let new_ew = EWSTR.replace("0", ew[i]);
-                contents = contents.replacen(EWSTR, new_ew.as_str(), 1);
-            } else {
-                let new_ew = EWSTR.replace("0", "-1");
-                contents = contents.replacen(EWSTR, new_ew.as_str(), 1);
-            }
-
-            if yy[i] != "0" {
-                let new_yy = YYSTR.replace("0", yy[i]);
-                contents = contents.replacen(YYSTR, new_yy.as_str(), 1);
-            } else {
-                let new_yy = YYSTR.replace("0", "-1");
-                contents = contents.replacen(YYSTR, new_yy.as_str(), 1);
-            }
-        }
-
-        let new_ew = EWSTR.replace("0", "-1");
-        contents = contents.replace(new_ew.as_str(), EWSTR);
-        let new_yy = YYSTR.replace("0", "-1");
-        contents = contents.replace(new_yy.as_str(), YYSTR);
-
-        contents
+        Ok(Response::builder().body(contents))
     } else {
-        unreachable!("程序错误，请联系作者！");
+        unreachable!("错误的POST请求，请联系作者修复！")
     }
 }
 
-fn record(name: &str, ew: [&str; ELEMN], yy: [&str; ELEMN]) {
-    let name: String = name.to_string() + ".ini";
+fn record(name: String, ew: String, yy: String) {
+    let name: String = name + ".ini";
     let path_string = "./".to_string() + &name;
     if Path::new(&path_string).exists() {
         fs::remove_file(&path_string).unwrap();
@@ -124,11 +87,7 @@ fn record(name: &str, ew: [&str; ELEMN], yy: [&str; ELEMN]) {
         .open(path_string)
         .unwrap();
 
-    let mut content = ew[0].to_string();
-    for elem in ew[1..].iter().chain(yy.iter()) {
-        content = content + &"," + elem;
-    }
-
+    let content = ew + &"\r\n" + &yy[..];
     file.write_all(content.as_bytes()).unwrap();
 }
 
